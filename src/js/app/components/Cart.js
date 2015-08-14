00e7c93d9193028454b56466e2cd1e00724b40a1
nicolas-daniel <nicolas.daniel.29@gmail.com>
2015-08-14 11:13:52 +0200

    add remove item button for each product in cart

--- src/js/app/components/Cart.js
+++ src/js/app/components/Cart.js
@@ -17,16 +17,34 @@ export default class Cart {
 	}
 
 	render() {
+		let that = this
+
 		return (
 			<div className={'cart ' + (this.props.enabled ? 'cart--enabled ' : ' ') + (this.props.visible ? 'cart--visible' : '')} ref='cart'>
 				<div className='cart__count' onClick={this.open}>Cart — <span>{this.props.count}</span></div>
 				<div className='cart__content'>
 					<div className='cart__close' onClick={this.close}>x</div>
+					<ul className='payment__products'>
+						{Object.keys(this.props.products).map(function(id){
+							let product = that.props.products[id];
+							return (
+								<li key={id} className='payment__product'>
+									{product.city} - {product.year}
+									<div>quantity: <span>{product.quantity}</span></div>
+									<div onClick={that.removeItem.bind(this, id)}>Remove item</div>
+								</li>
+							)
+						})}
+					</ul>
 					<div className='cart__subtotal'>Subtotal: <span>{this.props.total}</span></div>
 					<a href='#/payment' className='cart__checkout'>Check out</a>
 				</div>
 			</div>
 		)
+	}
+
+	removeItem(id) {
+		CartActions.removeFromCart(id)
 	}
 
 }
