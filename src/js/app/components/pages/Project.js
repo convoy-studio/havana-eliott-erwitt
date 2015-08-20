7a622c7a0e3fe76b6b584f269b7325d0c23e3abb
nicolas-daniel <nicolas.daniel.29@gmail.com>
2015-08-20 23:39:55 +0200

    add zoom on slideshow

--- src/js/app/components/pages/Project.js
+++ src/js/app/components/pages/Project.js
@@ -26,9 +26,11 @@ export default class Project extends Page {
 			print: {}
 		};
 
+		this.zoom = false
 		this.slideshowPrints = {}
 		this.action = 'init'
 		this.currentIndex = 0
+		this.toggleZoomBinded = this.toggleZoom.bind(this)
 		this.toggleStoryBinded = this.toggleStory.bind(this)
 		this._prevBinded = this.prev.bind(this)
 		this._nextBinded = this.next.bind(this)
@@ -53,7 +55,7 @@ export default class Project extends Page {
 
 	render() {
 		let that = this
-		let name, bio, city, story, forSale, year, projectTitle, projectDesc = []
+		let name, bio, city, year, story, forSale, url, projectTitle, projectDesc = []
 		if (this.state.artist) {
 			name = this.state.artist.name
 			bio = this.state.artist.bio
@@ -82,6 +84,7 @@ export default class Project extends Page {
 			year = this.slideshowPrints.current.year
 			story = this.slideshowPrints.current.desc
 			forSale = this.slideshowPrints.current.forSale
+			url = '#/shop/' + this.slideshowPrints.current._id
 		}
 
 		return (
@@ -98,9 +101,9 @@ export default class Project extends Page {
 					</p>
 					<div className='project__discover'><div className='arrow'></div></div>
 					<div className='project__slideshow'>
-						<div className='project__prints'>
+						<div className='project__prints' onClick={this.toggleZoomBinded}>
 							{Object.keys(this.slideshowPrints).map((index) => {
-								let file = this.slideshowPrints[index].file + '_min.jpg'
+								let file = this.slideshowPrints[index].file + '.jpg'
 								let status = index
 								return (
 									<div className={'project__print project__print--'+status} key={index}><img className='project__image' src={'./assets/images/prints/'+file}></img></div>
@@ -109,10 +112,10 @@ export default class Project extends Page {
 							<div className='project__story text text--big'>
 								<p>{story}</p>
 							</div>
-							<div className='project__nav'>
-								<div className='project__prev' onClick={this._prevBinded}><div className='arrow'></div></div>
-								<div className='project__next' onClick={this._nextBinded}><div className='arrow'></div></div>
-							</div>
+						</div>
+						<div className='project__nav'>
+							<div className='project__prev' onClick={this._prevBinded}><div className='arrow'></div></div>
+							<div className='project__next' onClick={this._nextBinded}><div className='arrow'></div></div>
 						</div>
 						<div className='project__footer'>
 							<div className='project__section'>
@@ -128,7 +131,7 @@ export default class Project extends Page {
 								<div className='project__reveal button button--left button--small' onClick={this.toggleStoryBinded}>The story</div>
 								{(() => {
 									if (forSale) return (
-										<div className='project__buy button button--right button--small'>Buy print</div>
+										<a href={url} className='project__buy button button--right button--small'>Buy print</a>
 									)
 								})()}
 							</div>
@@ -137,6 +140,33 @@ export default class Project extends Page {
 				</section>
 			</div>
 		)
+	}
+
+	toggleZoom() {
+		if (this.zoom) {
+			Tweenmax.staggerTo([
+				dom('.front-container'),
+				dom('.project__share'),
+				dom('.project__infos'),
+				dom('.project__reveal'),
+				dom('.cart')
+			], 0.4, {opacity: 1});
+			Tweenmax.to(dom('.project__prints'), 0.4, {scale: 1, ease: Power2.easeOut});
+			Tweenmax.to(dom('.project__prev'), 0.4, {x: 0, ease: Power2.easeOut});
+			Tweenmax.to(dom('.project__next'), 0.4, {x: 0, ease: Power2.easeOut});
+		} else {
+			Tweenmax.staggerTo([
+				dom('.front-container'),
+				dom('.project__share'),
+				dom('.project__infos'),
+				dom('.project__reveal'),
+				dom('.cart')
+			], 0.4, {opacity: 0});
+			Tweenmax.to(dom('.project__prints'), 0.4, {scale: 1.35, ease: Power2.easeOut});
+			Tweenmax.to(dom('.project__prev'), 0.4, {x: -70, ease: Power2.easeOut});
+			Tweenmax.to(dom('.project__next'), 0.4, {x: 70, ease: Power2.easeOut});
+		}
+		this.zoom = !this.zoom
 	}
 
 	toggleStory() {
