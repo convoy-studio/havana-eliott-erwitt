0ce3a64509e609d1245d19c5a420550c2a0ad4d4
Jérôme Macias <jerome.macias@gmail.com>
2016-05-31 22:38:38 +0200

    Fix home by partialy reverting a previous commit

--- app/js/actions/appActions.js
+++ app/js/actions/appActions.js
@@ -15,8 +15,14 @@ var AppActions = {
             actionType: AppConstants.WINDOW_RESIZE,
             item: { windowW:windowW, windowH:windowH }
         });
-    }
+    },
 
+    disableSplash: () => {
+        AppDispatcher.handleViewAction({
+            actionType: AppConstants.SPLASH_DISABLED,
+            item: false
+        });
+    }
 }
 
 export default AppActions;
