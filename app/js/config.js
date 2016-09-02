2c136cbf901b8c08acfeaae2a03bffd38801b550
Rich Choy <rich@it-consultis.com>
2016-09-02 17:01:07 +0800

    fix syntax error in config.js

--- app/js/config.js
+++ app/js/config.js
@@ -8,17 +8,11 @@ if(typeof window !== 'undefined') {
 	let getUrl = window.location;
 	let baseUrl = getUrl.protocol + "//" + getUrl.host;
 	config = {
-		'siteurl': baseUrl
+		'siteurl': baseUrl,
 		// 'siteurl': 'http://localhost:3000'
 		// 'siteurl': 'http://havana.hi9.fr'
 		// 'siteurl': 'http://www.havana-fellowship.com',
 
-    prestashop: {
-      key: env.PRESTASHOP_API_KEY,
-      scheme: env.PRESTASHOP_API_SCHEME || 'https',
-      host: env.PRESTASHOP_API_HOST,
-      root: env.PRESTASHOP_API_ROOT || '/api',
-    },
 	};
 }
 
