97c18229479cf40b9c15b0c1d025547fa06b455d
Rich Choy <rich@it-consultis.com>
2016-09-02 15:53:11 +0800

    whitespace correction

--- app/js/utils/prestashop.js
+++ app/js/utils/prestashop.js
@@ -8,89 +8,89 @@ import lang from './lang';
  * @return {String}
  */
 const stringify = (query) => {
-  let encode = encodeURIComponent;
-  let pairs = _.map(query, (v, k) => `${encode(k)}=${encode(v)}`);
+	let encode = encodeURIComponent;
+	let pairs = _.map(query, (v, k) => `${encode(k)}=${encode(v)}`);
 
-  return pairs.join('&');
+	return pairs.join('&');
 };
 
 
 class Client {
 
-  /**
-   * Return instance configuration defaults
-   * @param void
-   * @return {Object}
-   */
-  defaults () {
-    return {
-      basePath: '/shop.php',
-      fetch: fetch, //--> for testing
-    };
-  }
+	/**
+	 * Return instance configuration defaults
+	 * @param void
+	 * @return {Object}
+	 */
+	defaults () {
+		return {
+			basePath: '/shop.php',
+			fetch: fetch, //--> for testing
+		};
+	}
 
-  /**
-   * @param {Object} options
-   */
-  constructor (options={}) {
-    this.options = {...this.defaults(), ...options};
-  }
+	/**
+	 * @param {Object} options
+	 */
+	constructor (options={}) {
+		this.options = {...this.defaults(), ...options};
+	}
 
-  /**
-   * Send a GET request
-   * @param 
-   */
-  get (uri, query={}) {
-    let url = this.url(uri, query);
-    let fopts = this.createFetchOptions({method: 'GET'});
+	/**
+	 * Send a GET request
+	 * @param 
+	 */
+	get (uri, query={}) {
+		let url = this.url(uri, query);
+		let fopts = this.createFetchOptions({method: 'GET'});
 		let fetch = this.options.fetch;
 
-    return fetch(url, fopts).then((response) => {
-      this.validateResponse(response);
-      return response;
-    });
-  }
+		return fetch(url, fopts).then((response) => {
+			this.validateResponse(response);
+			return response;
+		});
+	}
 
-  /**
-   * @param {String} uri - the request path
-   * @param {Object} query - query parameters
-   * @return {String}
-   */
-  url (uri, query) {
-    let opts = this.options;
-    let url = path.join(opts.basePath, uri);
+	/**
+	 * @param {String} uri - the request path
+	 * @param {Object} query - query parameters
+	 * @return {String}
+	 */
+	url (uri, query) {
+		let opts = this.options;
+		let url = path.join(opts.basePath, uri);
 
-    if (!lang.empty(query)) {
-      url += '?' + stringify(query);
-    }
+		if (!lang.empty(query)) {
+			url += '?' + stringify(query);
+		}
 
-    return url;
-  }
+		return url;
+	}
 
-  /**
-   * @param {Object} augments
-   * @return {Object}
-   */
-  createFetchOptions (augments={}) {
-    let opts = this.options;
+	/**
+	 * @param {Object} augments
+	 * @return {Object}
+	 */
+	createFetchOptions (augments={}) {
+		let opts = this.options;
 
-    return {
-      mode: 'cors',
-      cache: 'default',
-      ...augments,
-    };
-  }
+		return {
+			mode: 'cors',
+			cache: 'default',
+			...augments,
+		};
+	}
 
-  /**
-   * @param {Response} response
-   * @return void
-   * @throws Error
-   */
-  validateResponse (response) {
-    if (!response.ok) {
-      throw new Error('got non-2XX HTTP response status');
-    }
-  }
+	/**
+	 * @param {Response} response
+	 * @return void
+	 * @throws Error
+	 */
+	validateResponse (response) {
+		if (!response.ok) {
+			throw new Error('got non-2XX HTTP response status');
+		}
+	}
 
 }
 
