c93386cbf30dd8e040be74ea338fb98e50de326a
Rich Choy <rich@it-consultis.com>
2016-10-13 14:41:12 +0800

    comments

--- server/controllers/cache.js
+++ server/controllers/cache.js
@@ -3,6 +3,7 @@ export default {
 	flush: {
 
 		// Flush the LRU cache; see Hapi server entrypoint script.
+		// TODO: put security in front of this route
 		handler: (req, reply) => {
 			let {cache} = req.server.app;
 
