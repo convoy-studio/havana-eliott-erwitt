0cd814a77741c4321053a2a18908425026d8fe7a
Rich Choy <rich@it-consultis.com>
2016-10-17 09:56:53 +0800

    fix webpack HMR on docker

--- gulp/utils/webpack.server.js
+++ gulp/utils/webpack.server.js
@@ -1,3 +1,14 @@
+const env = process.env;
+
+let headers = {
+	'Access-Control-Allow-Origin': 'http://localhost:3000',
+	'Access-Control-Allow-Headers': 'X-Requested-With',
+};
+
+if (env.WEBPACK_SERVER_PROXY) {
+	headers['Access-Control-Allow-Origin'] = 'http://' + env.WEBPACK_SERVER_PROXY;
+}
+
 const serverOptions = {
 	contentBase: './js/',
 	publicPath: '/js/',
@@ -5,10 +16,7 @@ const serverOptions = {
 	hot: true,
 	stats : false,
 	historyApiFallback: true,
-	headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
-      'Access-Control-Allow-Headers': 'X-Requested-With'
-    }
+	headers: headers,
 };
 
 export default serverOptions;
